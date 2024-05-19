81659ce8598c77b989583ed15151aa9a8bc1f1c5
Gustavo Henric <152925867+GustavoHenric@users.noreply.github.com>
2024-05-19 18:55:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const display = document.getElementById('display');
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    display.value += operator;
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        alert('Expressão inválida');
+        clearDisplay();
+    }
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+
